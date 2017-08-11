db2367c18c83f1f9e9fce92e746904ba22a56c91
Mohamed Hamza <mhamza15@gmail.com>
2017-08-10 22:45:05 -0400

    Rewrote script, added env vars

--- script.py
+++ script.py
@@ -2,45 +2,69 @@ import twitter
 import praw
 from time import sleep
 import re
-import html
+import HTMLParser
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+import threading
+
+load_dotenv(join(dirname(__file__), '.env'))
+
+def setInterval(func, sec):
+    def funcWrapper():
+        setInterval(func, sec) 
+        func()  
+    t = threading.Timer(sec, funcWrapper)
+    t.start()
+    return t
 
 def removeURL(status):
     return(re.sub(r"https://t.co\S+", "", status))
-        
-twitterApi = twitter.Api(consumer_key="CONSUMER_KEY",
-                         consumer_secret="CONSUMER_SECRET",
-                         access_token_key="ACCESS_TOKEN_KEY",
-                         access_token_secret="ACCESS_TOKEN_SECRET")
-redditApi = praw.Reddit(client_id='CLIENT_ID',
-                        client_secret="CLIENT_SECRET", 
-                        password='PASSWORD',
-                        user_agent='USER_AGENT',
-                        username='USERNAME')
 
-subreddit = redditApi.subreddit("SUBREDDIT")
+try:
+  twitterApi = twitter.Api(
+    consumer_key=os.environ['CONSUMER_KEY'],
+    consumer_secret=os.environ['CONSUMER_SECRET'],
+    access_token_key=os.environ['ACCESS_TOKEN_KEY'],
+    access_token_secret=os.environ['ACCESS_TOKEN_SECRET']
+  )
+  reddit = praw.Reddit(
+    client_id=os.environ['CLIENT_ID'],
+    client_secret=os.environ['CLIENT_SECRET'], 
+    password=os.environ['PASSWORD'],
+    user_agent='twitter-to-reddit by /u/twitterToRedditBot',
+    username=os.environ['USERNAME']
+  )
+except KeyError:
+  print('Error! Please include the relevant authentication as environment variables')
+  exit()
+  
+account = raw_input('Hi! What account would you like to monitor?\n')
+subreddit = raw_input('What subreddit would you like to post to?\n')
 
-statuses = twitterApi.GetUserTimeline(screen_name="SCREEN_NAME",
-                                      count=1,
-                                      exclude_replies=True,
-                                      include_rts=False)
-oldStatuses = statuses
+print('\nMonitoring: @' + account)
+print('Posting to: r/' + subreddit + '\n')
 
-while (True):
-    if (oldStatuses == statuses):
-        sleep(60)
-        statuses = twitterApi.GetUserTimeline(screen_name="SCREEN_NAME",
-                                              count=1,
-                                              exclude_replies=True,
-                                              include_rts=False)
-        continue
-    oldStatuses = statuses
-    subreddit.submit(title=html.unescape(removeURL(statuses[0].text)), 
-                     url="https://twitter.com/SCREEN_NAME/status/" + str(statuses[0].id), 
-                     send_replies=False)
-    sleep(60)
-    statuses = twitterApi.GetUserTimeline(screen_name="SCREEN_NAME",
-                                          count=1,
-                                          exclude_replies=True,
-                                          include_rts=False)
+tweets = []
 
-    
+def checkTweets():
+  global tweets
+
+  print('Checking for new tweets...')
+  newTweets = twitterApi.GetUserTimeline(
+    screen_name=account,
+    count=5,
+    exclude_replies=True,
+    include_rts=False
+  )
+  
+  if (newTweets != tweets):
+    tweets = newTweets
+    reddit.subreddit(subreddit).submit(
+      title=HTMLParser.HTMLParser().unescape(removeURL(tweets[0].text)),
+      url='https://twitter.com/' + account + '/status/' + str(tweets[0].id),
+      send_replies=False
+    )
+    print('New tweet! Posted to r/' + subreddit + '\n')
+
+setInterval(checkTweets, 300)  
